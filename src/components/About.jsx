c97aac9bcd1f5440ab3b24b1173c9f2b6db37562
Simon Bruce <simonbrucepuma@gmail.com>
2022-05-06 22:06:31 -0400

    sd

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,17 +15,17 @@ function About() {
         </div>
         <div className="max-w-[1000px] w-full gap-8 px-4 text-xl">
           <div className="">
-            <p className="px-2">
-              Hello, I am Simon. I am a computer engineer student at Florida International University, graduating in Summer 2022.</p>
-            <p className="px-2">
+            <p className="px-2 text-justify">
+              Hello, I am Simon. I am a computer engineering student at Florida International University, graduating in Summer 2022.</p>
+            <p className="px-2 text-justify">
               Since the beginning of my degree, I have been learning different programming languages such as Java, C, and C++, but I am very passionate about
-              websites and web applications, and I am
+              websites and web applications. I am
               now focused on web development,
-              learning every day more about HTML, CSS, Javascript, and React
+              learning more every day about HTML, CSS, Javascript, and React,
               and planning to start studying in the near future back-end development to become a full-stack developer.</p>
-              <p className="px-2">
+            <p className="px-2 text-justify">
               I believe in lifelong learning and continuous improvement, so here I am, trying my best.
-              Please take a look at my most recent projects and don't hesitate to contact me to connect or if you have any question
+              Please take a look at my most recent projects and don't hesitate to contact me to connect or if you have any questions
             </p>
           </div>
         </div>
