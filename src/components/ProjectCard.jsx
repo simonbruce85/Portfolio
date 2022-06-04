0c42df78281fcf8e1388d98901c253a49d0687f8
Simon Bruce <simonbruce@Simons-Mac-mini.local>
2022-06-03 20:28:52 -0400

    scrollbar

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,24 +2,25 @@ import React from 'react'
 
 const ProjectCard = (props) => {
   return (
-    < div >
-      { /*Grid Item*/}
-      <div style={{ backgroundImage: `url(${props.backimg})` }} className='shadow-lg shadow-[#040c16] group container rounded-t-md flex justify-center items-center mx-auto flex-col content-div'></div>
-      <div className='bg-[#222525] rounded-b flex-col '>
-        <p className='text-2xl font-bold text-white pt-4 pl-8'>
+    <div className=' w-[40%] inline-block relative p-2 rounded-b' >
+      <div className='rounded-b'>
+      <img id="imgWork" className='bg-fixed h-[250px] w-[400px]' src={props.backimg} alt={"hello"} />
+      </div>
+      <div className='bg-[#222525] rounded-b min-h-[300px] flex flex-col p-4 justify-between'>
+        <p className='text-2xl font-bold text-white whitespace-normal'>
           {props.projectname}
         </p>
-        <div className='flex pt-4 pl-8 gap-4'>
+        <div className='flex justify-between'>
           <p>{props.tech1}</p>
           <p>-</p>
           <p>{props.tech2}</p>
           <p>-</p>
           <p>{props.tech3}</p>
         </div>
-        <div className=' pt-4 px-8 justify-content '>
-          <p>{props.description}</p>
+        <div className='whitespace-normal'>
+          <p >{props.description}</p>
         </div>
-        <div className='text-center flex pl-6 pt-4 pb-8'>
+        <div className='text-center flex'>
           <a href={props.demo} target="_blank" >
             <button className='text-center rounded-lg h-8 px-4 m-2 bg-white text-gray-700 font-bold text-md' id="button-contact">Demo</button>
           </a>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,24 +4,37 @@ import Crypto from '../assets/crypto-price.jpg'
 import TodoList from '../assets/Todo-List.png'
 import ProjectCard from './ProjectCard'
 
+import {MdChevronLeft, MdChevronRight} from "react-icons/md"
+
 const Work = () => {
+
+  const slideLeft = () =>{
+    var slider = document.getElementById('slider')
+    slider.scrollLeft = slider.scrollLeft - 500;
+}
+
+const slideRight = () =>{
+    var slider = document.getElementById('slider')
+    slider.scrollLeft = slider.scrollLeft + 500;
+}
+
   return (
     <div name='work' className='w-full text-gray-300 bg-[#041F31]'>
-      <div className='max-w-[1000px] mx-auto py-32 p-4 flex flex-col justify-center w-full h-full '>
+      <div className='max-w-[1000px] mx-auto py-32 p-4 flex flex-col justify-center '>
         <div className='pb-8'>
           <p className='text-4xl font-bold text-gray-300 inline border-b-4 border-[#00F5A0]  '>Projects</p>
           <p className='py-6'>Check out some of my recent work!</p>
         </div>
-        { /*Container*/}
-        <div className='grid sm:grid-cols-2 md:grid-cols-2 gap-16'>
-          { /*Grid Item*/}
-          <ProjectCard backimg={For3x} projectname={"For3x Funding"} tech1={"HTML"} tech2={"CSS"} tech3={"Bootstrap"} description={"Landing pafe for For3x Funding, a NFT project with solidity integrated"} github={"https://github.com/simonbruce85/Web"} demo={"https://for3x-funding.netlify.app"}/>
-          <ProjectCard backimg={Crypto} projectname={"Crypto Currency Price Tracker"} tech1={"React"} tech2={"JavaScript"} tech3={"Tailwind"} description={"Website oriented to track the price of thousands of cryptocurrencies using CoinGecko API"} github={"https://github.com/simonbruce85/cryptoAPI"} demo={"https://crypto-price-api.netlify.app"}/>
-          <ProjectCard backimg={For3x} projectname={"Growing Beyond Earth"} tech1={"Arduino"} tech2={"Fusion360"} tech3={"Harwdare"} description={"Finalists in NASA sponsored contest Growing Beyond Earth as the FIU team"} github={"/"} demo={"https://makeprojects.com/project/fiu-autonomous-farmer---year-3-collegiate-entry"} />
-          <ProjectCard backimg={TodoList} projectname={"To-do List"} tech1={"React"} tech2={"JavaScript"} tech3={"Firebase"} description={"To-do List application based on firebase interactiont to add, modify and delete tasks"} github={"https://github.com/simonbruce85/TodoFirebase"} demo={"https://simons-to-do-list.netlify.app/"} />
-          
+        <div className="flex items-center group relative">
+        <MdChevronLeft onClick={slideLeft} className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40}/>
+        <div id={'slider' } className=" w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide ">
+        <ProjectCard backimg={For3x} projectname={"For3x Funding"} tech1={"HTML"} tech2={"CSS"} tech3={"Bootstrap"} description={"Landing pafe for For3x Funding, a NFT project with solidity integrated"} github={"https://github.com/simonbruce85/Web"} demo={"https://for3x-funding.netlify.app"}/>
+            <ProjectCard backimg={Crypto} projectname={"Crypto Currency Price Tracker"} tech1={"React"} tech2={"JavaScript"} tech3={"Tailwind"} description={"Website oriented to track the price of thousands of cryptocurrencies using CoinGecko API"} github={"https://github.com/simonbruce85/cryptoAPI"} demo={"https://crypto-price-api.netlify.app"}/>
+            <ProjectCard backimg={For3x} projectname={"Growing Beyond Earth"} tech1={"Arduino"} tech2={"Fusion360"} tech3={"Harwdare"} description={"Finalists in NASA sponsored contest Growing Beyond Earth as the FIU team"} github={"/"} demo={"https://makeprojects.com/project/fiu-autonomous-farmer---year-3-collegiate-entry"} />
+            <ProjectCard backimg={TodoList} projectname={"To-do List"} tech1={"React"} tech2={"JavaScript"} tech3={"Firebase"} description={"To-do List application based on firebase interactiont to add, modify and delete tasks"} github={"https://github.com/simonbruce85/TodoFirebase"} demo={"https://simons-to-do-list.netlify.app/"} />
         </div>
-
+        <MdChevronRight onClick={slideRight} className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer hidden group-hover:block' size={40}/>
+    </div>
       </div>
     </div>
   )
